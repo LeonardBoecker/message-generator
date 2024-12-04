6225462c0a240076172101164dedc79232c40d07
Leonard <lmvboecker@hotmail.com>
2024-12-04 17:20:28 -0300

    added new file

---
+++ main.js
@@ -0,0 +1,12 @@
+//Project message generator
+const pronounWords = [
+    'your ',
+    'you ',
+];
+const otherWords = [
+    'will get that.',
+    'got be kidding me.',
+    'needs to make the difference.',
+    'know what it need to be done.',
+    'shirt needs to be washed.'
+];
