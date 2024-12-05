db40258f14cb1b02930c001bb5fd61b6d453c40e
Leonard <lmvboecker@hotmail.com>
2024-12-05 09:03:32 -0300

    Main code

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-//Project message generator
-/*as it required to be ramdom phrase
-first step was to discover how phrases are made
-second give some shots 
-then make it random 
-then run it to test
-*/
+// Project: Message Generator
+// Description: Generates random phrases by combining pronouns, objects, and predicates.
 const randomPhrase = {
     pronouns: [
-        //index one refers to personal pronouns
+        //Personal pronouns
         [
-            //Some space after the pronouns
+            // Space after to avoid word concatenation
             "You ",
             "He ",
             "She ",
             "They ",
             "It "
         ],
-        //index 2 refers to possessive pronouns
+        //Possessive pronouns
         [
             "My ",
             "Your ",
@@ -26,7 +21,7 @@ const randomPhrase = {
         ]
     ],
     predicates: [
-        // some predicates that I wanted to used 
+        // General predicates (can be used with any pronoun)
         [
             "will get that.",
             "got to be kidding me.",
@@ -39,7 +34,7 @@ const randomPhrase = {
             "make the good out of people, keep so.",
             "make it right."
         ],
-        //starting with objects to make things different
+        // Objects (used only with possessive pronouns)
         [
             "bike ",
             "handgrip ",
@@ -54,13 +49,9 @@ const randomPhrase = {
 };
 
 function generateRandomPhrase() {
-    /*first ramdom describe which index will be 
-    because I have one for possessive and other for
-    personal pronouns and if I want to add more in future
-    will be possible becaause the first ramdom bellow*/
+    // Randomly select the pronoun type (0 for personal, 1 for possessive)
     const randomPronounType = Math.floor(Math.random() * randomPhrase.pronouns.length);
-    /*once Ive finished the pronous chosen I now need which pronoun use
-    then I randomize the chosen one before and set it in randomPronoun*/
+    // Randomly select a pronoun within the chosen type
     const randomPronoun = randomPhrase.pronouns[randomPronounType][Math.floor(Math.random() * randomPhrase.pronouns[randomPronounType].length)];
 
 
@@ -69,9 +60,9 @@ function generateRandomPhrase() {
     if (randomPronounType === 1) { // if Possessive pronoun cause it is in first index then
         // Select the object and for more fun ...>
         const randomObject = randomPhrase.predicates[1][Math.floor(Math.random() * randomPhrase.predicates[1].length)];
-        // ramdom the normal predicate after the object like his bike make it right
+        // Select a random predicate to follow the object
         const randomPredicate = randomPhrase.predicates[0][Math.floor(Math.random() * randomPhrase.predicates[0].length)];
-        // here bellow I have the concatenation of object with predicate
+        // Combine the object and predicate
         predicate = randomObject + randomPredicate;
 
 
@@ -79,9 +70,9 @@ function generateRandomPhrase() {
         predicate = randomPhrase.predicates[0][Math.floor(Math.random() * randomPhrase.predicates[0].length)];
 
     }
-    //by the end I finihs with both concatenation pronoun and predicate
+    // Return the complete phrase
     return randomPronoun + predicate;
 }
-
-console.log(generateRandomPhrase()); // Example usage
-console.log(generateRandomPhrase()); // Another example usage
+// Example usage:
+console.log(generateRandomPhrase());
+console.log(generateRandomPhrase()); 
